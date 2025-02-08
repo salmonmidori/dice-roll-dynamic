073cfb226e883d38b66be82356654df3ab4a00c8
Amy May Nguyen <anguye10@uchicago.edu>
2025-02-08 01:20:38 +0000

    Made templates view subfolder

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,11 +1,11 @@
 class DiceController < ApplicationController
   def show_homepage
-    render({:template => "homepage"})
+    render({:template => "templates/homepage"})
   end
 
   def roll_dice
     @number_of_dice = params["num_dice"].to_i
     @number_of_sides = params["num_sides"].to_i
-    render({:template => "dice_results"})
+    render({:template => "templates/dice_results"})
   end
 end
